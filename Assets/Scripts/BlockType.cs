527fd0a53219d8aa89afc57b95fbd0845ac3dcde
ncomendant <nicholas.comendant@gmail.com>
2018-06-18 14:08:44 -0700

    Replaced arrow block text with arrows

--- Assets/Scripts/BlockType.cs
+++ Assets/Scripts/BlockType.cs
@@ -8,10 +8,10 @@ public class BlockType {
     public static readonly BlockType S = new BlockType(KeyCode.S, "S", 2f);
     public static readonly BlockType D = new BlockType(KeyCode.D, "D", 1f);
 
-    public static readonly BlockType UP = new BlockType(KeyCode.UpArrow, "U", -1f);
-    public static readonly BlockType LEFT = new BlockType(KeyCode.LeftArrow, "L", -2f);
-    public static readonly BlockType DOWN = new BlockType(KeyCode.DownArrow, "D", -3f);
-    public static readonly BlockType RIGHT = new BlockType(KeyCode.RightArrow, "R", -4f);
+    public static readonly BlockType UP = new BlockType(KeyCode.UpArrow, "\u25b2", -1f);
+    public static readonly BlockType LEFT = new BlockType(KeyCode.LeftArrow, "\u25c0", -2f);
+    public static readonly BlockType DOWN = new BlockType(KeyCode.DownArrow, "\u25bc", -3f);
+    public static readonly BlockType RIGHT = new BlockType(KeyCode.RightArrow, "\u25b6", -4f);
 
     public static readonly BlockType[] VALUES = new BlockType[]{BlockType.W, BlockType.A, BlockType.S, BlockType.D, BlockType.UP, BlockType.LEFT, BlockType.DOWN, BlockType.RIGHT};
 
