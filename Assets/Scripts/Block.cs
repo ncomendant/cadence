db8ae635abb6ee88d48bc0a276a51d2166175389
ncomendant <nicholas.comendant@gmail.com>
2018-06-18 11:33:50 -0700

    Randomly generates moving blocks towards player.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+	}
+
+    public void Init(int typeIndex)
+    {
+        BlockType type = BlockType.VALUES[typeIndex];
+        transform.Translate(0f, 0f, type.position);
+        GetComponentInChildren<TextMesh>().text = type.text;
+    }
+}
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
 
+    public float blockInterval;
+    private float blockCooldown;
+
 	// Use this for initialization
 	void Start () {
-        print("Hello world!");
+        blockCooldown = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        blockCooldown -= Time.deltaTime;
+        if (blockCooldown <= 0f)
+        {
+            MakeRandomBlock();
+            blockCooldown = blockInterval;
+        }
 	}
+
+    private void MakeRandomBlock()
+    {
+        int index = Random.Range(0, 8);
+        GameObject blockObj = (GameObject)Instantiate(Resources.Load("Block"));
+        Block block = blockObj.GetComponent<Block>();
+        block.Init(index);
+    }
 }
